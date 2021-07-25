022837d4b353f7f629d9ab5f9a95ffdaf0c8893c
Josh <OneHappyForever@users.noreply.github.com>
2021-07-25 09:55:02 +0700

    Update acme.sh

--- data/conf/acme.sh
+++ data/conf/acme.sh
@@ -2,4 +2,10 @@
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
 export AWS_DNS_SLOWRATE=2
-acme.sh --issue -d us-tls.teacher2070.com --dns dns_aws --dnssleep 30
+acme.sh --issue -d DOMAIN --dns dns_aws --dnssleep 30
+
+ROOTDIR="/root/ssl/conf"
+
+mkdir $ROOTDIR/active
+
+acme.sh --install-cert -d DOMAIN --key-file $ROOTDIR/privkey.pem --fullchain-file $ROOTDIR/fullchain.pem --reloadcmd "docker restart $(docker ps -a -q)"
